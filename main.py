f8b092fe82d44a067d69a0f94250bc804590488d
Rafael Garcia <TthanderBR@gmail.com>
2023-09-18 18:22:37 -0300

    system itens and spells

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
+spell = {
+	"fire_ball": {
+		'name'			: "fire_ball",
+		'damage'		: "2d6",
+		'type_damege'	: "fire",
+	},
+	"star_rain": {
+		'name'			: "star_rain",
+		'damage'		: "2d6",
+		'type_damage'	: "light",
+	}
+}
+iten = {
+	"base_sword":{
+		'name'			: "base_sword",
+		'damage'		: "1d6", 
+		'type_damage'	: "physic",
+	}
+}
 
 class sheet_token():
 	def __init__(self) -> None:
-		char_name	= ""
-		char_class	= ""
-		char_job	= ""
+		self.char_name	= ""
+		self.char_class	= ""
+		self.char_job	= ""
 
-		attributes = {
+		self.attributes = {
 			'strength'		: 0,
 			'dexterity'		: 0,
 			'constitution'	: 0,
 			'intelligence'	: 0,
 			'wisdom'		: 0,
-			'charisma'		: 0
+			'charisma'		: 0,
 		}
-		modifiers = {
+		self.modifiers = {
 			'stre_mod'		: 0,
 			'dext_mod'		: 0,
 			'cons_mod'		: 0,
@@ -21,34 +40,19 @@ class sheet_token():
 			'wisd_mod'		: 0,
 			'char_mod'		: 0,
 		}
-		inventory	= []
-		spells		= []
-		skills		= []
+		self.inventory	= []
+		self.spells		= []
+		self.skills		= []
+
+	def set_class(self, arg_class):
+		match arg_class:
+			case "mage":
+				self.char_class = "Mage"
+				self.spells.append(spell['star_rain'])
+				self.spells.append(spell['fire_ball'])
+
+				self.inventory.append(iten['base_sword'])
 
 
-char_sheet = {
-	'name'		: "",
-	'class'		: "",
-	'job'		: "",
-	
-	'attributes': {
-		'strength'		: 0,
-		'dexterity'		: 0,
-		'constitution'	: 0,
-		'intelligence'	: 0,
-		'wisdom'		: 0,
-		'charisma'		: 0
-	},
-	'modifiers' : {
-		'stre_mod'		: 0,
-		'dext_mod'		: 0,
-		'cons_mod'		: 0,
-		'inte_mod'		: 0,
-		'wisd_mod'		: 0,
-		'char_mod'		: 0,
-	},
-	
-	'inventory'	: [],
-	'spells'	: [],
-	'skills'	: [],
-}
+
+
